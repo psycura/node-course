7f122b182724e1082a89c401f625ca566e04fd3b
Eugene Alitz <alitz@emg-soft.com>
2017-08-31 14:27:06 +0300

    before deploy

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ app.get ( '/about', ( req, res ) => {
     } );
 } );
 
+app.get ( '/projects', ( req, res ) => {
+    res.render ( 'projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'this is my projects',
+    } );
+} );
+
 app.get ( '/bad', ( req, res ) => {
     res.send ( {
         errorMessage: 'Unable to connect'
